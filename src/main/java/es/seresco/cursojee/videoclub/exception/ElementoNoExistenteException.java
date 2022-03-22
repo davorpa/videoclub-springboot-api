50bff1c842ab26308795abdd3971357e94642ff1
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-03-22 23:27:13 +0100

    Add excepciones de negocio lanzadas por los services y/o controllers

---
+++ src/main/java/es/seresco/cursojee/videoclub/exception/ElementoNoExistenteException.java
@@ -0,0 +1,7 @@
+package es.seresco.cursojee.videoclub.exception;
+
+public class ElementoNoExistenteException extends Exception {
+
+	private static final long serialVersionUID = 6004871815354527714L;
+
+}
---
+++ src/main/java/es/seresco/cursojee/videoclub/exception/PeticionInconsistenteException.java
@@ -0,0 +1,7 @@
+package es.seresco.cursojee.videoclub.exception;
+
+public class PeticionInconsistenteException extends Exception {
+
+	private static final long serialVersionUID = -4634077372519724144L;
+
+}
