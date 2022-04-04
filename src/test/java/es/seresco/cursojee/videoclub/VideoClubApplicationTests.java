bb24e79d5e4525b15ba2c108e1d89d61a25639f3
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-04-04 15:30:52 +0200

    Add general tests

---
+++ src/test/java/es/seresco/cursojee/videoclub/VideoClubApplicationTests.java
@@ -0,0 +1,13 @@
+package es.seresco.cursojee.videoclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
